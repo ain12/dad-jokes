0c33b8f9a300d3910510ca0e4a7489f92c2b5dfd
Ainhoa <ainhoaactic@gmail.com>
2021-06-14 13:16:57 +0200

    Level 1 - show random joke on screen clicking a button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from "react"
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  const [joke, setJoke] = useState("");
+
+  const getJoke = () => {
+    fetch("https://icanhazdadjoke.com/", {
+      headers: {
+        "Accept": "application/json"
+      }
+    })
+      .then(res => res.json())
+      .then(data => setJoke(data.joke))
+  }
+
   return (
     <div className="App">
+      <p>{joke}</p>
+      <button onClick={getJoke}>Next Joke</button>
     </div>
-  );
+  )
 }
 
 export default App;
