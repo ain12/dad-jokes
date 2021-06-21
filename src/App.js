ba9832d41bc3943432f3ff22c25e670c7af7bef0
Ainhoa <ainhoaactic@gmail.com>
2021-06-21 11:11:19 +0200

    level 3 ex: 5 - link welcomepage with jokeapp page with routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React, { useState } from "react";
+//import React, { useState } from "react";
 //import './components/App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import JokeApp from "./components/JokeApp";
 import WelcomePage from "./Welcome"
 
 function App() {
-  const [welcome, setWelcome] = useState(true);
+  //const [welcome, setWelcome] = useState(true);
   return (
-    <React.Fragment>
-      {welcome ? <WelcomePage content={welcome} onEnterClick={() => setWelcome(false)} /> : <JokeApp></JokeApp>}
-    </React.Fragment>
+    /* <React.Fragment>
+       {welcome ? <WelcomePage content={welcome} onEnterClick={() => setWelcome(false)} /> : <JokeApp></JokeApp>}
+     </React.Fragment> */
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <WelcomePage />
+        </Route>
+        <Route path="/JokeApp">
+          <JokeApp />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 
